5bad27d2a300250235bdc86a457f881c69ad2570
Mohamed Atef <eng.mohamedatef98@gmail.com>
2019-03-20 21:30:41 +0200

    Stack Init

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import gi
-gi.require_version("Gtk", "3.0")
-from gi.repository import Gtk
+from window import *
 
-window = Gtk.Window(title="Hello World")
-window.show()
-window.connect("destroy", Gtk.main_quit)
-Gtk.main()
+window = OSWindow()
+window.run()
---
+++ window.py
@@ -0,0 +1,28 @@
+import gi
+gi.require_version('Gtk', '3.0')
+from gi.repository import Gtk
+
+class OSWindow(Gtk.Window):
+    def __init__(self):
+        Gtk.Window.__init__(self, title="OS PROJECT")
+        self.box = Gtk.Box(spacing=15)
+
+        vbox = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=6)
+        self.add(vbox)
+
+        stack = Gtk.Stack()
+        stack.set_transition_type(Gtk.StackTransitionType.SLIDE_LEFT_RIGHT)
+        stack.set_transition_duration(1000)
+
+        stack_switcher = Gtk.StackSwitcher()
+        stack_switcher.set_stack(stack)
+        vbox.pack_start(stack_switcher, True, True, 0)
+        vbox.pack_start(stack, True, True, 0)
+
+
+        self.connect('destroy', Gtk.main_quit)
+        self.show_all()
+
+
+    def run(self):
+        Gtk.main()
