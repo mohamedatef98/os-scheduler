62e92bc4504a3b983f768ad049d4d7ce556bc30e
MohamedEshmawy <36339453+MohamedEshmawy@users.noreply.github.com>
2019-04-10 18:03:14 +0200

    fixed PriorityProcess class

--- processes/PriorityProcess.py
+++ processes/PriorityProcess.py
@@ -3,5 +3,5 @@ from .NormalProcess import NormalProcess
 class PriorityProcess(NormalProcess):
 
     def __init__(self, name, arrival, time, priority):
-        NormalProcess.__init__(name, arrival, time)
+        NormalProcess.__init__(self,name, arrival, time)
         self.priority = priority
