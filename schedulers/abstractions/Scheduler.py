e0b0451d33502d5201ce338f0eeb0029f3a6d6a6
Eshmawy <mohamed.eshmawy@yahoo.com>
2019-04-14 15:12:24 -0700

    added function to calculate currently waiting processes

--- schedulers/abstractions/Scheduler.py
+++ schedulers/abstractions/Scheduler.py
@@ -1,6 +1,18 @@
 class Scheduler:
-    def __init__(self, queue):
-        pass
+	def __init__(self, queue):
+		pass
 
-    def schedule(self):
-        pass
+	def schedule(self):
+		pass
+
+	def getWaitingProcNum(sorted_queue_processes, counter):
+		waitingProcNum = 0;
+
+		for p in sorted_queue_processes:
+			if p.arrival > counter:
+				break
+			if p.arrival < counter:
+				waitingProcNum++
+
+		waitingProcNum-- #remove the current executing process
+		return waitingProcNum
